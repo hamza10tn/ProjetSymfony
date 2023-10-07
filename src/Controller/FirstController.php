09f624c7b780817e0aac6691f6ab4347994fc38f
hamza <ayari.hamza1@esprit.tn>
2023-10-07 16:15:08 +0100

    rev

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -52,15 +52,13 @@ class FirstController extends AbstractController
     {
         $authors = array(
             array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
-            'victor.hugo@gmail.com ', 'nb_books' => 0),
+            'victor.hugo@gmail.com ', 'nb_books' => 1),
             array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
             ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
             array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
-            'taha.hussein@gmail.com', 'nb_books' => 300),
+            'taha.hussein@gmail.com', 'nb_books' => 100),
             );
         
-        return $this->render('3A16/list.html.twig',[
-            'a'=>$authors,
-        ]);
+        return $this->render('3A16/list.html.twig',['a'=>$authors, ]);
     }
 }
